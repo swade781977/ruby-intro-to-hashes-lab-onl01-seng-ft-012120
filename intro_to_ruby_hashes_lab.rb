344fa8d53ab6f375114a086e5a19d1ffc2e74fef
Chris Wade <swade781977@yahoo.com>
2020-01-27 17:19:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,17 +22,27 @@ def monopoly_with_second_tier
 	    :names => {}
 	  }
 	}
-
 end
 
 def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
-	    :names => {}
+	    :pieces => 4,
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
+	      
+	    },
+	    :names => {
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_o_railroad => {},
+	      :shortline => {}
 	    }
-	  }
-	}
-
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
